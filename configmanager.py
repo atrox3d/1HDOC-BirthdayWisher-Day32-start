311155aca43aca36c1dac2455e5aa87bce9010ca
atrox3d <atrox3d@gmail.com>
2021-02-22 07:46:55 +0100

    improved configmanager.py

--- configmanager.py
+++ configmanager.py
@@ -7,53 +7,67 @@ USER = "user"
 PASSWORD = "password"
 SERVER = "server"
 
-config = None
+# _config = None
+# _mail_config = None
 
 
-def _load(email, filepath=CONFIG_FILE):
+def _load(email=None, filepath=CONFIG_FILE):
     """ get key from json """
-    global config
+    # global _config, _mail_config
     try:
         with open(filepath, "r") as config_file:
-            config = json.load(config_file)
+            _config = json.load(config_file)
     except FileNotFoundError as FNFE:
         print(f"_load|KO|the file '{filepath}' does not exist")
         return False
     else:
-        config = config.get(email)
-        if config:
+        if email is None:
+            email = next(iter(_config))
+        _mail_config = _config.get(email)
+        if _mail_config:
             print(f"_load|OK|mail '{email}' found in file '{filepath}'")
-            return True
+            return _mail_config
         else:
             print(f"_load|KO|mail '{email}' NOT found in file '{filepath}'")
-            return False
+            return None
 
 
-def get_config_pretty(email, filepath=CONFIG_FILE):
-    if _load(email, filepath):
+def get_config_pretty(email=None, filepath=CONFIG_FILE):
+    config = _load(email, filepath)
+    if config is not None:
         return json.dumps(config, indent=4)
     return None
 
 
-def get_config(email, filepath=CONFIG_FILE):
-    if _load(email, filepath):
+def get_config(email=None, filepath=CONFIG_FILE):
+    config = _load(email, filepath)
+    if config is not None:
         return config
     return None
 
 
-def get_user(email, filepath=CONFIG_FILE):
-    if _load(email, filepath):
-        return get_config(email).get(USER)
+def get_user(email=None, filepath=CONFIG_FILE):
+    config = _load(email, filepath)
+    if config is not None:
+        return config.get(USER)
     return None
 
 
-def get_password(email, filepath=CONFIG_FILE):
-    if _load(email, filepath):
-        return get_config(email).get(PASSWORD)
+def get_password(email=None, filepath=CONFIG_FILE):
+    config = _load(email, filepath)
+    if config is not None:
+        return config.get(PASSWORD)
     return None
 
 
-def get_server(email, filepath=CONFIG_FILE):
-    if _load(email, filepath):
-        return get_config(email).get(SERVER)
+def get_server(email=None, filepath=CONFIG_FILE):
+    config = _load(email, filepath)
+    if config is not None:
+        return config.get(SERVER)
     return None
+
+
+def get_email(filepath=CONFIG_FILE):
+    config = _load(None, filepath)
+    if config is not None:
+        return next(iter(config))
